494a87fdf1a0be85531774a78036dd68dbf6e438
Manar27 <Manar200032@gmail.com>
2023-11-18 14:40:54 +0200

    CAN Driver

---
+++ ARM/CAN/CAN_config.h
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name		 : CAN_config.h
+ Author		 : Manar Hashem - Martina Mofied
+ Date		 : Oct 31, 2023
+ Version	 : 1.0
+ Description : CAN_config header file
+ ============================================================================
+*/
+#ifndef CAN_CONFIG_H
+#define CAN_CONFIG_H
+
+
+
+
+#endif
